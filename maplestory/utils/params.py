6e787741944d3a20659522b761d65cd4d8b909af
Seo, Jang-won <nanpuhaha@gmail.com>
2024-03-12 10:08:58 +0900

    🔧 Update every_element_contains logic for None list and improve readability
    🔧 every_element_contains 함수의 None 리스트에 대한 로직을 업데이트하고 가독성을 개선합니다

--- maplestory/utils/params.py
+++ maplestory/utils/params.py
@@ -6,8 +6,12 @@ def remove_none_values(d: dict[str, Any] | None) -> dict[str, Any] | None:
 
 
 def every_element_contains(lis: list[str], key: str) -> bool:
-    return all(key in v for v in lis if v is not None)
+    if lis is None:
+        return False
+    return any(v is not None for v in lis) and all(
+        key in v for v in lis if v is not None
+    )
 
 
 def every_element_not_contains(lis: list[str], key: str) -> bool:
-    return all(key not in v for v in lis if v is not None)
+    return all(key not in v for v in lis if v is not None) if key is not None else True
